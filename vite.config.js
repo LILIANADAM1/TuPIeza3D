7abac24d81e03b0f183bae0b40ec3fd872e40f71
LILIANADAM1 <mmoreno.liliana@gmail.com>
2025-06-12 14:22:35 +0200

    Update Vite config and index.html for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TuPIeza3D/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096
+  }
 })
